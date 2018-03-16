f28455810aed29f33c9bdff61975d8748228d10e
AraceliGS <aragutarras@gmail.com>
2018-03-16 11:33:54 -0500

    first commit

---
+++ src/componentes/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from './searchbar'
+// Falta continuar
+class App extends Component {
+  render() {
+    return (
+      <SearchBar />
+    );
+  }
+}
+
+export default App;
